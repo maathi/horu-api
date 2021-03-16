8cf67b5186964e830692d4ce11989812f128c0fe
maathi <mahdi221b@gmail.com>
2021-03-16 08:59:44 +0100

    simple server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+const requestListener = function (req, res) {
+  res.writeHead(200)
+  let referer = req.headers.referer
+  let ip = req.connection.remoteAddress
+
+  res.end(ip + referer)
+}
+
+const server = http.createServer(requestListener)
+server.listen(8080)
